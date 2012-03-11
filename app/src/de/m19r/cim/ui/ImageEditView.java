1d92f718eaf9cdb3991528a53a8405516df33f97
friedger <friedger@gmail.com>
2012-03-11 16:35:31 +0100

    surfaceholder added

---
+++ app/src/de/m19r/cim/ui/ImageEditView.java
@@ -0,0 +1,212 @@
+package de.m19r.cim.ui;
+
+import java.util.List;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
+
+public class ImageEditView extends SurfaceView implements
+		SurfaceHolder.Callback {
+
+	class ImageEditThread extends Thread {
+		Context mContext;
+		SurfaceHolder mSurfaceHolder;
+		private boolean mRun;
+		private List<ICommand> mCommands;
+
+		public ImageEditThread(SurfaceHolder surfaceHolder, Context context,
+				Handler handler) {
+			mSurfaceHolder = surfaceHolder;
+			mContext = context;
+
+		}
+
+		/**
+		 * Starts the game, setting parameters for the current difficulty.
+		 */
+		public void doStart() {
+			synchronized (mSurfaceHolder) {
+
+			}
+		}
+
+		/**
+		 * Pauses the physics update & animation.
+		 */
+		public void pause() {
+			synchronized (mSurfaceHolder) {
+				// set state to paused
+			}
+		}
+
+		@Override
+		public void run() {
+			while (mRun) {
+				Canvas c = null;
+				try {
+					c = mSurfaceHolder.lockCanvas(null);
+					synchronized (mSurfaceHolder) {
+						//
+						doDraw(c);
+					}
+				} finally {
+					// do this in a finally so that if an exception is thrown
+					// during the above, we don't leave the Surface in an
+					// inconsistent state
+					if (c != null) {
+						mSurfaceHolder.unlockCanvasAndPost(c);
+					}
+				}
+			}
+		}
+
+		/**
+		 * Dump game state to the provided Bundle. Typically called when the
+		 * Activity is being suspended.
+		 * 
+		 * @return Bundle with this view's state
+		 */
+		public Bundle saveState(Bundle map) {
+			synchronized (mSurfaceHolder) {
+
+			}
+			return map;
+		}
+
+		/**
+		 * Used to signal the thread whether it should be running or not.
+		 * Passing true allows the thread to run; passing false will shut it
+		 * down if it's already running. Calling start() after this was most
+		 * recently called with false will result in an immediate shutdown.
+		 * 
+		 * @param b
+		 *            true to run, false to shut down
+		 */
+		public void setRunning(boolean b) {
+			mRun = b;
+		}
+
+		/* Callback invoked when the surface dimensions change. */
+		public void setSurfaceSize(int width, int height) {
+			// synchronized to make sure these all change atomically
+			synchronized (mSurfaceHolder) {
+//				mCanvasWidth = width;
+//				mCanvasHeight = height;
+//
+//				// don't forget to resize the background image
+//				mBackgroundImage = Bitmap.createScaledBitmap(mBackgroundImage,
+//						width, height, true);
+			}
+		}
+
+		/**
+		 * Draws the ship, fuel/speed bars, and background to the provided
+		 * Canvas.
+		 */
+		private void doDraw(Canvas canvas) {
+			for (ICommand cmd : mCommands) {
+				mCimController(canvas, cmd);
+			}
+		}
+
+	}
+
+	/** Handle to the application context, used to e.g. fetch Drawables. */
+	private Context mContext;
+
+	/** Pointer to the text view to display "Paused.." etc. */
+	private TextView mStatusText;
+
+	/** The thread that actually draws the animation */
+	private ImageEditThread thread;
+
+	public ImageEditView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+
+		// register our interest in hearing about changes to our surface
+		SurfaceHolder holder = getHolder();
+		holder.addCallback(this);
+
+		// create thread only; it's started in surfaceCreated()
+		thread = new ImageEditThread(holder, context, new Handler() {
+			@Override
+			public void handleMessage(Message m) {
+				mStatusText.setVisibility(m.getData().getInt("viz"));
+				mStatusText.setText(m.getData().getString("text"));
+			}
+		});
+
+		setFocusable(true); // make sure we get key events
+	}
+
+	/**
+	 * Fetches the animation thread corresponding to this LunarView.
+	 * 
+	 * @return the animation thread
+	 */
+	public ImageEditThread getThread() {
+		return thread;
+	}
+
+	/**
+	 * Standard window-focus override. Notice focus lost so we can pause on
+	 * focus lost. e.g. user switches to take a call.
+	 */
+	@Override
+	public void onWindowFocusChanged(boolean hasWindowFocus) {
+		if (!hasWindowFocus)
+			thread.pause();
+	}
+
+	/**
+	 * Installs a pointer to the text view used for messages.
+	 */
+	public void setTextView(TextView textView) {
+		mStatusText = textView;
+	}
+
+	/* Callback invoked when the surface dimensions change. */
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
+		thread.setSurfaceSize(width, height);
+	}
+
+	/*
+	 * Callback invoked when the Surface has been created and is ready to be
+	 * used.
+	 */
+	public void surfaceCreated(SurfaceHolder holder) {
+		// start the thread here so that we don't busy-wait in run()
+		// waiting for the surface to be created
+		thread.setRunning(true);
+		thread.start();
+	}
+
+	/*
+	 * Callback invoked when the Surface has been destroyed and must no longer
+	 * be touched. WARNING: after this method returns, the Surface/Canvas must
+	 * never be touched again!
+	 */
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		// we have to tell thread to shut down & wait for it to finish, or else
+		// it might touch the Surface after we return and explode
+		boolean retry = true;
+		thread.setRunning(false);
+		while (retry) {
+			try {
+				thread.join();
+				retry = false;
+			} catch (InterruptedException e) {
+			}
+		}
+	}
+}
