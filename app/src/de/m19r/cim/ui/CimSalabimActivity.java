d5248fbc8f891594ad95f915d1567f6d0e8c7d46
friedger <friedger@gmail.com>
2012-03-11 12:42:19 +0100

    lib added

--- app/src/de/m19r/cim/ui/CimSalabimActivity.java
+++ app/src/de/m19r/cim/ui/CimSalabimActivity.java
@@ -1,13 +1,16 @@
-package de.m19r.cim;
+package de.m19r.cim.ui;
 
+import de.m19r.cim.R;
+import de.m19r.cim.R.layout;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 public class CimSalabimActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_main);
     }
 }
