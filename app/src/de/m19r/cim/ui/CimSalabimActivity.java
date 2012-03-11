fb742b5368d0a950d3e121eff506c8c2f111dd97
friedger <friedger@gmail.com>
2012-03-11 14:10:17 +0100

    dataconnection

--- app/src/de/m19r/cim/ui/CimSalabimActivity.java
+++ app/src/de/m19r/cim/ui/CimSalabimActivity.java
@@ -2,9 +2,14 @@ package de.m19r.cim.ui;
 
 import java.io.IOException;
 
+import jibe.sdk.client.JibeIntents;
 import jibe.sdk.client.simple.authentication.AuthenticationHelper;
 import jibe.sdk.client.simple.session.DatagramSocketConnection;
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
 import de.m19r.cim.R;
@@ -16,6 +21,7 @@ public class CimSalabimActivity extends Activity {
 	
 	private DatagramSocketConnection mConnection;
 	private AuthenticationHelper mAuthHelper;
+	private final static int AUTHENTICATING_DIALOG = 1;
 	
 	/** Called when the activity is first created. */
 	@Override
@@ -71,5 +77,66 @@ public class CimSalabimActivity extends Activity {
 		}
 
 	}
+	
+	private void resetConnection() {
+		/*
+		 * This may be called by callbacks, and may therefore not necessarily be
+		 * run within the UI context. In order to be able to update the screen,
+		 * force this to run as part of the UI thread.
+		 */
+		runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				//disableUiButtons();
+			}
+		});
 
+		createConnection();
+	}
+
+	private void createConnection() {
+		if (mConnection != null) {
+			
+			try {
+				mConnection.close();
+			} catch (IOException e) {
+				Log.w(LOG_TAG, "Failed to close connection.");
+				e.printStackTrace();
+			}
+			unregisterReceiver(mReceiver);
+		}
+		registerReceiver(mReceiver, new IntentFilter(
+				JibeIntents.ACTION_INCOMING_SESSION + '.'
+						+ JibeApplication.APP_ID));
+
+		mConnection = new DatagramSocketConnection(this, mConnStateListener);
+		
+	}
+	
+	private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+
+		@Override
+		public void onReceive(Context context, final Intent intent) {
+			runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					incomingSession(intent);
+				}
+			});
+		}
+	};
+
+	
+	private boolean incomingSession(Intent intent) {
+		try {
+			// Attach incoming intent to connection.
+			mConnection.attachIncomingSession(intent);
+			mPacketGenerator.setIsSender(false);
+			setUiButtonsForIncomingConnection();
+			return true;
+		} catch (IllegalArgumentException iex) {
+			Log.w(LOG_TAG, "Wrong intent");
+		}
+		return false;
+	}
 }
