cddd07f41d316d327320de6418935c1568981551
friedger <friedger@gmail.com>
2012-03-11 12:35:18 +0100

    skeleton

---
+++ app/src/de/m19r/cim/CimSalabimActivity.java
@@ -0,0 +1,13 @@
+package de.m19r.cim;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CimSalabimActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
